1f164409528f8354d142feae8c09b6debf848e1a
STHAOPHAN <phansongthao203@gmail.com>
2023-06-26 13:59:50 +0700

    update unit test to see red alert

--- src/test/java/org/helloworld/mathutil/core/test/MathUtilityAdvancedTest.java
+++ src/test/java/org/helloworld/mathutil/core/test/MathUtilityAdvancedTest.java
@@ -24,7 +24,7 @@ public class MathUtilityAdvancedTest {
                                {1, 1}, 
                                {2, 2}, 
                                {4, 24}, 
-                               {5, 120}};
+                               {5, 12000}};
         return  testData;
     }
     
