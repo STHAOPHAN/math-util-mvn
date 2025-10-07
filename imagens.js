6a8789581133f2e0a95ec05507a915384f28c584
Phan Song Thảo <133019917+STHAOPHAN@users.noreply.github.com>
2025-10-08 01:23:21 +0700

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -1,5 +1,6 @@
 [
-  { "url": "https://raw.githubusercontent.com/STHAOPHAN/math-util-mvn/main/image.png", chunk: [1627, 963], coords: [581, 535] }
+  { "url": "https://raw.githubusercontent.com/STHAOPHAN/math-util-mvn/main/image.png", "chunk": [1627, 963], "coords": [581, 535] }
 ]
+
 
 
