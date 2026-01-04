c8b8988847ee4e6be1339c060d82da87b538b718
STHAOPHAN <phansongthao203@gmail.com>
2026-01-04 17:08:20 +0700

    add image8

--- imagens.js
+++ imagens.js
@@ -5,7 +5,8 @@
   { "url": "https://raw.githubusercontent.com/STHAOPHAN/math-util-mvn/main/image4.png", "chunk": [1625, 963], "coords": [330, 479] },
   { "url": "https://raw.githubusercontent.com/STHAOPHAN/math-util-mvn/main/image5.png", "chunk": [1625, 964], "coords": [478, 198] },
   { "url": "https://raw.githubusercontent.com/STHAOPHAN/math-util-mvn/main/image6.png", "chunk": [1627, 964], "coords": [930, 75] },
-  { "url": "https://raw.githubusercontent.com/STHAOPHAN/math-util-mvn/main/image7.png", "chunk": [1628, 963], "coords": [689, 824] }
+  { "url": "https://raw.githubusercontent.com/STHAOPHAN/math-util-mvn/main/image7.png", "chunk": [1628, 963], "coords": [689, 824] },
+  { "url": "https://raw.githubusercontent.com/STHAOPHAN/math-util-mvn/main/image8.png", "chunk": [1626, 962], "coords": [534, 920] }
 ]
 
 
